6f9224e122042ac4cc4fd7ad4dc4bb8ce42a0a08
Flash6989 <flash6989@yandex.ru>
2022-03-30 22:56:12 +0400

    slider

--- js/index.js
+++ js/index.js
@@ -1,74 +1,138 @@
 const imagsTwoSlider = document.querySelectorAll("#works_slider img");
+const allImgSliderTwo = document.querySelectorAll(
+  ".works__slidercontainer img"
+);
+const parentListService = document.querySelector(".service__sliders");
 const firstSliderItems = document.querySelectorAll(".service__slider");
+const arrowLeft = document.querySelector(".arrow__left");
+const arrowRight = document.querySelector(".arrow__right");
+const sliderBlockTransform = document.querySelector(".slider__items");
 
 let timeSleepTwoSlider = 4000;
-
+let intervalSlider1 = null;
+let intervalSlider2 = null;
 window.onload = function () {
-  setInterval(
-    () => setImgPath(imagsTwoSlider),
-    timeSleepTwoSlider > 4000 ? 4000 : timeSleepTwoSlider
+  intervalSlider2 = setInterval(
+    () => nextImg(sliderBlockTransform),
+    timeSleepTwoSlider < 4000 ? 4000 : timeSleepTwoSlider
   );
-  setInterval(() => changeFirstSlider(firstSliderItems), 4000);
+
+  intervalSlider1 = setInterval(
+    () => changeFirstSlider(firstSliderItems, parentListService),
+    4000
+  );
 };
 
-function setImgPath(imags) {
-  const srcPath = [];
-  for (const item of imags) {
-    srcPath.push(item.getAttribute("src"));
-  }
-  for (const inx in imags) {
-    if (inx == imags.length - 1) {
-      imags[inx].setAttribute("src", srcPath[0]);
-      break;
-    }
-    imags[inx].setAttribute("src", srcPath[Number(inx) + 1]);
-  }
+// function setImgPath(imags) {
+//   const srcPath = [];
+//   for (const item of imags) {
+//     srcPath.push(item.getAttribute("src"));
+//     // sliderBlockTransform.style.transform = "translateX(-660px)";
+//   }
+//   console.log(srcPath);
+
+//   for (const inx in imags) {
+//     console.log(inx, "inx");
+
+//     if (inx == imags.length - 1) {
+//       imags[inx].setAttribute("src", srcPath[0]);
+//       break;
+//     }
+//     imags[inx].setAttribute("src", srcPath[Number(inx) + 1]);
+//   }
+// }
+
+// document
+//   .getElementById("works_slider")
+//   .addEventListener("click", function (event) {
+//     const src = event.target.getAttribute("src");
+//     if (!src) return false;
+
+//     if (src == imagsTwoSlider[0].getAttribute("src")) return false;
+
+//     while (imagsTwoSlider[0].getAttribute("src") != src) {
+//       setImgPath(imagsTwoSlider);
+//     }
+//   });
+
+//колво картинок всего
+let count = 4;
+//переменная для суммирования пикселей
+let position = 0;
+
+function clickImgChange(event) {
+  console.log(event);
+  const targetUrl = event.target.getAttribute("src");
+
+  allImgSliderTwo.forEach((item, id) => {
+    item.getAttribute("src") == targetUrl ? (position = 660 * id) : null;
+  });
+  handleChangeImg();
+}
+
+function handleChangeImg() {
+  clearInterval(intervalSlider2);
+
+  if (position > 660 * count - 1) position = 0;
+  sliderBlockTransform.style.transform = `translateX(${-position + "px"})`;
 }
 
 document
-  .getElementById("works_slider")
-  .addEventListener("click", function (event) {
-    const src = event.target.getAttribute("src");
-    if (!src) return false;
+  .querySelector(".works__smallfoto")
+  .addEventListener("click", (event) => clickImgChange(event));
 
-    if (src == imagsTwoSlider[0].getAttribute("src")) return false;
+console.log(allImgSliderTwo);
 
-    while (imagsTwoSlider[0].getAttribute("src") != src) {
-      setImgPath(imagsTwoSlider);
-    }
-  });
-
-let idx_two_slider = 0;
-function changeFirstSlider(arrItems) {
-  for (const item of arrItems) {
-    item.classList.remove("active__slider");
-  }
-  arrItems[idx_two_slider].classList.add("active__slider");
-
-
-  idx_two_slider++;
-  if (idx_two_slider > arrItems.length - 1) idx_two_slider = 0;
+function nextImg(sliderBlockTransform) {
+  position += 660;
+  if (position > 660 * count - 1) position = 0;
+  sliderBlockTransform.style.transform = `translateX(${-position + "px"})`;
 }
 
-function changeFirstSlider(arrItems) {
-  console.log(arrItems);
-
+function changeFirstSlider(arrItems, parentListService) {
   arrItems.forEach((element) => {
     if (element.classList.contains("active__slider")) {
-      console.log(element, "active");
       element.classList.remove("active__slider");
-      element.classList.add("no__activeleft");
-      return;
+
+      return element.classList.add("no__activeleft");
+    }
+
+    if (element.classList.contains("no__activeright")) {
+      element.classList.remove("no__activeright");
+      return element.classList.add("active__slider");
     }
     if (element.classList.contains("no__activeleft")) {
-      console.log(element, "left");
       element.classList.remove("no__activeleft");
-      element.classList.add("no__activeright");
-      return;
+      return element.classList.add("no__activeright");
     }
-    if (element.classList.contains("no__activeright")) {
-      console.log(element, "right");
+  });
+}
+
+function changeFirstSliderLeft(arrItems, parentListService) {
+  arrItems.forEach((element) => {
+    if (element.classList.contains("active__slider")) {
       element.classList.remove("active__slider");
-      return;
+
+      return element.classList.add("no__activeleft");
     }
-  })
+
+    if (element.classList.contains("no__activeright")) {
+      element.classList.remove("no__activeright");
+      return element.classList.add("active__slider");
+    }
+    if (element.classList.contains("no__activeleft")) {
+      element.classList.remove("no__activeleft");
+      return element.classList.add("no__activeright");
+    }
+  });
+}
+
+arrowLeft.addEventListener("click", (event) => {
+  clearInterval(intervalSlider1);
+});
+
+arrowRight.addEventListener("click", (event) => {
+  console.log("клик по правой стрелке");
+  changeFirstSlider(firstSliderItems);
+  clearInterval(intervalSlider1);
+});
